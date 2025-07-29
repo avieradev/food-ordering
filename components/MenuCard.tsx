28f0f10d9970f43698e0bb099be5e5b058f52103
Adrian <avieradev@gmail.com>
2025-07-29 18:05:14 +0200

    Add Cart feature, final video

--- components/MenuCard.tsx
+++ components/MenuCard.tsx
@@ -1,9 +1,14 @@
-import { View, Text, TouchableOpacity, Image, Platform } from "react-native";
+import { Text, TouchableOpacity, Image, Platform } from "react-native";
 import React from "react";
 import { MenuItem } from "@/type";
-import { appwriteConfig } from "@/lib/appwrite";
+import { useCartStore } from "@/store/cart.store";
 
-const MenuCard = ({ item: { image_url, name, price } }: { item: MenuItem }) => {
+const MenuCard = ({
+  item: { $id, image_url, name, price },
+}: {
+  item: MenuItem;
+}) => {
+  const { addItem } = useCartStore();
   return (
     <TouchableOpacity
       className={"menu-card"}
@@ -25,7 +30,11 @@ const MenuCard = ({ item: { image_url, name, price } }: { item: MenuItem }) => {
         {name}
       </Text>
       <Text className={"body-regular text-gray-200 mb-4"}>From ${price}</Text>
-      <TouchableOpacity onPress={() => {}}>
+      <TouchableOpacity
+        onPress={() =>
+          addItem({ id: $id, name, price, image_url, customizations: [] })
+        }
+      >
         <Text className={"paragraph-bold text-primary"}>Add to Cart</Text>
       </TouchableOpacity>
     </TouchableOpacity>
